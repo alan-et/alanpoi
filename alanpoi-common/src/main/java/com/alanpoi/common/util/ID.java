ea241c01b0a4bb02f7e4eafed8a841dcac6730f1
pengzhuoxun <pengzhuoxun168@163.com>
2020-04-03 10:29:22 +0800

    update

--- alanpoi-common/src/main/java/com/alanpoi/common/util/ID.java
+++ alanpoi-common/src/main/java/com/alanpoi/common/util/ID.java
@@ -8,11 +8,11 @@ import java.util.Random;
 import java.util.concurrent.atomic.AtomicInteger;
 
 /**
- * 64位分布式ID(Long)（支持高并发(每毫秒32767，超过并发数，排队1毫秒)）
+ * 64位分布式ID(Long)（支持高并发(单节点每毫秒16383，超过并发数，排队1毫秒)）
  * 支持时间  此方法可以支持139年(应该是目前除开UUID(UUID是128为字节，32位的字符，生成的ID无法排序)，支持时间最长的分布式算法)
  * 支持节点  为了更好的支持高并发，适量的降低了服务器节点数，此方法最多支持128(0-127)台服务器
  * 容错率高  高并发的场景往往不能保持原子性，一般很容易出现问题，因此此算法引入了AtomicInteger（原子类，CAS算法）来解决此问题
- * algorithm |42bit(timestamp)|15bit(version)|7bit(serverId)|
+ * algorithm |42bit(timestamp)|14bit(version)|7bit(serverId)|
  *
  * @author pengzhuoxun
  * @since 1.1.2
@@ -97,14 +97,14 @@ public class ID {
 
     private long getVersion() {
         version = atomicInt.incrementAndGet();
-        if (version == 0x7fff) atomicInt = new AtomicInteger(0);
+        if (version == 0x3fff) atomicInt = new AtomicInteger(0);
         if (version == 1 && modCount > 0) {
             modCount++;
             lock = String.valueOf(timestamp) + version;
             synchronized (lock) {
                 try {
                     if (lock.equals(String.valueOf((System.currentTimeMillis() - begin) << 21) + version)) {
-                        logger.warn("生成ID超过最大并发,系统正在排队处理,排队时间1毫秒,最大每毫米并发数:" + 0x7fff);
+                        logger.warn("生成ID超过最大并发,系统正在排队处理,排队时间1毫秒,最大每毫米并发数:" + 0x3fff);
                         Thread.sleep(1);
                         getTimestamp();
                     }
@@ -137,8 +137,8 @@ public class ID {
             System.out.println(idLong);
             list.add(idLong);
             i++;
-        } while (i < 0x7ffffff);
+        } while (i < 0x3fff);
 //        System.out.println(Long.MAX_VALUE);
-//        System.out.println(Long.valueOf((0x3ffffffffffL) << 21) | (0x7fff | 0L) << 7 | 0x7f);
+//        System.out.println(Long.valueOf((0x3ffffffffffL) << 21) | (0x3fff | 0L) << 7 | 0x7f);
     }
 }
